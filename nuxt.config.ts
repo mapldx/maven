c0a9de1c1fb3c4dc19e126fe4a5dfdde6c553dfe
mapldx <116471303+mapldx@users.noreply.github.com>
2023-03-07 17:29:25 -0500

    build: add Solana support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
+  vite: {
+      esbuild: {
+          target: 'esnext',
+      },
+      build: {
+          target: 'esnext',
+      },
+      optimizeDeps: {
+          include: ['@project-serum/anchor', '@solana/web3.js', 'buffer'],
+          esbuildOptions: {
+              target: 'esnext'
+          }
+      },
+      define: {
+          'process.env.BROWSER': true
+      }
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
---
+++ plugins/toast.ts
@@ -0,0 +1,11 @@
+import Toast, { PluginOptions, POSITION } from "vue-toastification"
+import "vue-toastification/dist/index.css"
+
+export default defineNuxtPlugin(nuxtApp => {
+    const options: PluginOptions = {
+        icon: false,
+        position: POSITION.BOTTOM_LEFT
+    }
+
+    nuxtApp.vueApp.use(Toast, options)
+})
