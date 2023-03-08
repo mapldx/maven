451bf67a403ac302da1978d12bf5fb91bd121182
mapldx <116471303+mapldx@users.noreply.github.com>
2023-03-07 20:36:12 -0500

    feat: add create form functionality

--- api/app.js
+++ api/app.js
@@ -5,8 +5,10 @@ const port = 80
 app.use(express.json());
 
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const { getFirestore, Timestamp, FieldValue, arrayUnion } = require('firebase-admin/firestore');
 const serviceAccount = require("../serviceAccountKey.json");
+
+var uniqid = require('uniqid'); 
 
 initializeApp({
   credential: cert(serviceAccount)
@@ -27,14 +29,69 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/api/auth', async (req, res) => {
-  console.log(req.body)
   const address = req.body.address
   const user = db.collection('users').doc(address)
+
   await user.set({
     address: address,
     active_on: Timestamp.now(),
   })
+
+  const documents = db.collection('documents').doc(address)
+  const doc = await documents.get()
+
+  if (!doc.exists) {
+    documents.set({
+      address: address,
+      forms: [],
+      sites: [],
+    })
+    console.log('Created new document for user')
+  }
+
   res.send('Hello World!')
+})
+
+app.post('/api/forms/create', async (req, res) => {
+  const address = req.body.address
+  const name = req.body.name
+  const desc = req.body.desc
+  const documents = db.collection('documents').doc(address)
+  const doc = await documents.get()
+
+  const formId = uniqid('form-', address)
+  if (doc.exists) {
+    await documents.update({
+      forms: FieldValue.arrayUnion(formId)
+    });
+  }
+
+  const form = db.collection('forms').doc(formId)
+  await form.set({
+    id: formId,
+    name: name,
+    desc: desc,
+    owner: address,
+    created: Timestamp.now(),
+    fields: [],
+    responses: [],
+    target: 'lily',
+    published: false,
+  })
+
+  res.send(formId)
+  return formId
+})
+
+app.get('/api/forms/:target', async (req, res) => {
+  var formList = []
+  const target = req.params.target
+  const forms = db.collection('forms')
+  const doc = await forms.where('target', '==', target).get()
+  doc.forEach(form => {
+    formList.push(form.id)
+  });
+  res.send(formList)
 })
 
 app.listen(port, () => {
--- client/pages/app.vue
+++ client/pages/app.vue
@@ -1,3 +1,257 @@
 <template>
-  Authenticated!
+  <div class="min-h-full">
+    <Disclosure as="nav" class="border-b border-gray-200 bg-white" v-slot="{ open }">
+      <div class="mx-auto max-w-7xl px-4 py-3 sm:px-6 lg:px-8">
+        <div class="flex h-16 justify-between">
+          <div class="flex">
+            <div class="flex flex-shrink-0 items-center">
+              <img class="block h-8 w-auto lg:hidden" src="/logo/default.png" alt="Your Company" />
+              <img class="hidden h-8 w-auto lg:block" src="/logo/default.png" alt="Your Company" />
+            </div>
+          </div>
+          <div class="hidden sm:ml-6 sm:flex sm:items-center">
+          <ClientOnly>
+            <WalletMultiButton />
+            <template #placeholder>
+              Loading
+            </template>
+          </ClientOnly>
+          </div>
+          <div class="-mr-2 flex items-center sm:hidden">
+            <ClientOnly>
+              <WalletMultiButton />
+              <template #placeholder>
+                Loading
+              </template>
+            </ClientOnly>
+          </div>
+        </div>
+      </div>
+
+      <DisclosurePanel class="sm:hidden">
+        <div class="space-y-1 pt-2 pb-3">
+          <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-indigo-50 border-indigo-500 text-indigo-700' : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800', 'block pl-3 pr-4 py-2 border-l-4 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+        </div>
+        <div class="border-t border-gray-200 pt-4 pb-3">
+          <div class="flex items-center px-4">
+            <div class="flex-shrink-0">
+              <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
+            </div>
+            <div class="ml-3">
+              <div class="text-base font-medium text-gray-800">{{ user.name }}</div>
+              <div class="text-sm font-medium text-gray-500">{{ user.email }}</div>
+            </div>
+            <button type="button" class="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+              <span class="sr-only">View notifications</span>
+              <BellIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div class="mt-3 space-y-1">
+            <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">{{ item.name }}</DisclosureButton>
+          </div>
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+
+    <div class="py-10">
+      <header>
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">Your Documents</h1>
+        </div>
+      </header>
+      <main>
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div class="px-4 py-8 sm:px-0">
+            <div class="">
+              <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                <li v-for="doc in docs" :key="doc.email" class="col-span-1 divide-y divide-gray-200 rounded-2xl bg-gray-200">
+                  <div class="flex w-full items-center justify-between space-x-6 p-6">
+                    <div class="flex-1 truncate">
+                      <div class="flex items-center space-x-3">
+                        <h3 class="truncate text-sm font-medium text-gray-900">{{ doc.name }}</h3>
+                        <span class="inline-block flex-shrink-0 rounded-full bg-green-100 px-2 py-0.5 text-xs font-medium text-green-800">{{ doc.role }}</span>
+                      </div>
+                      <p class="mt-1 truncate text-sm text-gray-500">{{ doc.title }}</p>
+                    </div>
+                    <img class="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" :src="doc.imageUrl" alt="" />
+                  </div>
+                  <div>
+                    <div class="-mt-px flex divide-x divide-gray-200">
+                      <div class="flex w-0 flex-1">
+                        <a :href="`mailto:${doc.email}`" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500">
+                          <PencilSquareIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                          <span class="ml-3">Edit</span>
+                        </a>
+                      </div>
+                      <div class="-ml-px flex w-0 flex-1">
+                        <a :href="`tel:${doc.telephone}`" class="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500">
+                          <ChatBubbleLeftEllipsisIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                          <span class="ml-3">Responses</span>
+                        </a>
+                      </div>
+                      <div class="-ml-px flex w-0 flex-1">
+                        <a :href="`tel:${doc.telephone}`" class="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500">
+                          <Cog6ToothIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                          <span class="ml-3">Settings</span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li class="col-span-1 divide-y divide-gray-200 rounded-2xl bg-gradient-to-r from-blue-300 via-blue-400 to-blue-300 p-1 shadow-xl">
+                  <div class="flex w-full items-center justify-between space-x-6 p-6">
+                    <div class="flex-1 truncate items-center justify-center text-center">
+                      <div class="flex items-center space-x-3 justify-center text-center">
+                        <h3 class="truncate text-sm font-bold text-white">Create a new document</h3>
+                      </div>
+                      <p class="mt-1 truncate text-sm text-white">Select document type</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div class="-mt-px flex divide-x divide-gray-200">
+                      <div class="flex w-0 flex-1" @click="openModal">
+                        <a class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-white hover:text-gray-500">
+                          <DocumentMagnifyingGlassIcon class="h-5 w-5 text-white" aria-hidden="true" />
+                          <span class="ml-3">Form</span>
+                        </a>
+                      </div>
+                      <div class="-ml-px flex w-0 flex-1">
+                        <a class="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-white hover:text-gray-500">
+                          <DevicePhoneMobileIcon class="h-5 w-5 text-white" aria-hidden="true" />
+                          <span class="ml-3">One Page Site</span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+  <TransitionRoot appear :show="isOpen" as="template">
+    <Dialog as="div" @close="closeModal" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black bg-opacity-25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <DialogTitle
+                as="h3"
+                class="text-lg font-medium leading-6 text-gray-900"
+              >
+                Create a new form
+              </DialogTitle>
+              <div class="mt-4">
+                <form class="flex flex-col space-y-2" @submit.prevent="submitForm">
+                  <label for="username" class="text-sm font-medium text-gray-700">Give it a name</label>
+                  <input id="username" v-model="formName" type="text" class="border border-gray-400 rounded-md py-2 px-3">
+                  <label for="username" class="text-sm font-medium text-gray-700">Short description (limited to 40 chars.)</label>
+                  <textarea id="username" v-model="formDesc" type="text" class="border border-gray-400 rounded-md py-2 px-3" 
+                    maxlength="40" 
+                    placeholder="This will be displayed under the form name"
+                    />
+                  <div class="pt-3">
+                    <button
+                      type="submit"
+                      class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      @click="newForm"
+                    >
+                      Continue
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
+
+<script setup>
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { ChatBubbleLeftEllipsisIcon, DocumentMagnifyingGlassIcon, DevicePhoneMobileIcon, PencilSquareIcon, Cog6ToothIcon } from '@heroicons/vue/20/solid'
+
+import { WalletMultiButton, useWallet } from 'solana-wallets-vue'
+import { useToast } from "vue-toastification";
+
+import axios from 'axios'
+
+import { ref } from 'vue'
+import {
+  Disclosure,
+  DisclosurePanel,
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/vue'
+
+const { publicKey } = useWallet()
+const { wallet, program } = useAnchor()
+
+const toast = useToast();
+watch(wallet, () => {
+    if(!wallet.value?.publicKey) {
+      toast('Successfully disconnected wallet!');
+      navigateTo('/')
+    }
+})
+
+const isOpen = ref(false)
+
+function closeModal() {
+  isOpen.value = false
+}
+
+function openModal() {
+  isOpen.value = true
+}
+
+const formName = ref('')
+const formDesc = ref('')
+
+async function newForm() {
+  const data = {
+    address: wallet.value?.publicKey,
+    name: formName.value,
+    desc: formDesc.value,
+  }
+  axios.post('http://localhost/api/forms/create', data)
+    .then(res => {
+      toast('Successfully created form!');
+      navigateTo(`/forms/${res.data}`)
+    })
+    .catch(err => {
+      toast.error('Error creating form!');
+    })
+}
+</script>
