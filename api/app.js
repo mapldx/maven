abfe1ed116628505117116d9cdd99ffa7692175e
mapldx <116471303+mapldx@users.noreply.github.com>
2023-03-29 20:15:04 -0400

    fix: add route protection

--- api/app.js
+++ api/app.js
@@ -17,14 +17,19 @@ initializeApp({
 const db = getFirestore();
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  const allowedOrigins = ['https://usemaven.app', 'https://layer.usemaven.app', 'http://localhost:3000'];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   next();
 });
-
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 })
